d47a62decfebbdf1e8ec6c57d17dab6ac3252b93
halcharger <allen.firth@gmail.com>
2014-11-20 15:56:13 +0200

    updating ref to Adaptivesystems assemblies and adding RentifyVersionController

---
+++ Rentify.WebServer/Controllers/RentifyVersionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Rentify.WebServer.Controllers
+{
+    public class RentifyVersionController : ApiController
+    {
+        [HttpGet]
+        [Route("api/version")]
+        public IEnumerable<string> GetAssemblyVersions()
+        {
+            var versions = new List<string>();
+            var webApiAssembly = Assembly.GetAssembly(typeof(RentifyVersionController));
+            versions.Add(string.Format("{0} [{1}]", webApiAssembly.FullName, webApiAssembly.GetName().Version));
+            versions.AddRange(webApiAssembly.GetReferencedAssemblies().Select(item => string.Format("{0} [{1}]", item.FullName, item.Version)));
+
+            return versions;
+        }
+    }
+}
